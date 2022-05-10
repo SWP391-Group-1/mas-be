a49dab228af6c4ebd620f5fc67a5dc741443ddac
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-05-11 06:53:33 +0700

    Add Nuget Packages

---
+++ src/MAS.API/Helpers/ApiConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace MAS.API.Helpers
+{
+    public class ApiConstants
+    {
+        public const string ServiceName = "mas";
+        public const string FriendlyServiceName = "Mentor Appointment Scheduling Application";
+    }
+}
