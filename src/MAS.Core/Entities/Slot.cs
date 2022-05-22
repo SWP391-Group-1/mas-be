9e2d28a4f7aab7d6932139a405c66863e787adfe
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-05-22 12:09:51 +0700

    Modify Slot

--- src/MAS.Core/Entities/Slot.cs
+++ src/MAS.Core/Entities/Slot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MAS.Core.Entities
 {
@@ -8,5 +9,6 @@ namespace MAS.Core.Entities
         public MasUser Mentor { get; set; }
         public DateTime StartTime { get; set; }
         public DateTime FinishTime { get; set; }
+        public ICollection<Appointment> Appointments { get; set; }
     }
 }
