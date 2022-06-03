90a969578360db1e4dfb8cecb1abd26949257162
NguyenTheNhan <nhanntse140758@fpt.edu.vn>
2022-06-03 10:51:53 +0700

    .

--- src/MAS.Core/Constants/Error/ErrorMessages.cs
+++ src/MAS.Core/Constants/Error/ErrorMessages.cs
@@ -6,6 +6,5 @@
         public const string Exist = "Existing ";
         public const string SaveFail = "Can not save!";
         public const string NotAllowModify = "You not allow to modify this infomation!";
-        public const string NotLoggedIn = "You aren't logged in!";
     }
 }
