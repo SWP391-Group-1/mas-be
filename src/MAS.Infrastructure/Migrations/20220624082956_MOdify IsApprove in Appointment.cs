a011c3bd1ccd15fb20932fe4de7675571d3aeeb8
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-06-24 15:31:59 +0700

    Update Appointment IsApprove

---
+++ src/MAS.Infrastructure/Migrations/20220624082956_MOdify IsApprove in Appointment.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MAS.Infrastructure.Migrations
+{
+    public partial class MOdifyIsApproveinAppointment : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsApprove",
+                table: "Appointments",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsApprove",
+                table: "Appointments",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+    }
+}
