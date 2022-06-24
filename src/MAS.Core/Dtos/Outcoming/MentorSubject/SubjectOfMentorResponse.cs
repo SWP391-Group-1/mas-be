45bb3212c0d55f4681b596a15a8aacb70b8ff52f
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-06-24 08:35:32 +0700

    Update SubjectOfMentorResponse

--- src/MAS.Core/Dtos/Outcoming/MentorSubject/SubjectOfMentorResponse.cs
+++ src/MAS.Core/Dtos/Outcoming/MentorSubject/SubjectOfMentorResponse.cs
@@ -2,9 +2,9 @@
 
 public class SubjectOfMentorResponse
 {
-    public string Id { get; set; }
     public string MajorId { get; set; }
 
+    public string Code { get; set; }
     public string Title { get; set; }
     public string Description { get; set; }
 }
