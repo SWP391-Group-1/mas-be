37d8c6698736bc6a25afb490b76a1259059facb2
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-06-29 09:03:16 +0700

    Update MentorSubject

--- src/MAS.Core/Dtos/Outcoming/MentorSubject/MentorSubjectResponse.cs
+++ src/MAS.Core/Dtos/Outcoming/MentorSubject/MentorSubjectResponse.cs
@@ -1,11 +1,13 @@
-﻿namespace MAS.Core.Dtos.Outcoming.MentorSubject;
+﻿using MAS.Core.Dtos.Outcoming.Subject;
+
+namespace MAS.Core.Dtos.Outcoming.MentorSubject;
 
 public class MentorSubjectResponse
 {
     public string Id { get; set; }
 
     public string SubjectId { get; set; }
-    public SubjectOfMentorResponse Subject { get; set; }
+    public SubjectDetailResponse Subject { get; set; }
 
     public string BriefInfo { get; set; }
     public bool IsActive { get; set; }
