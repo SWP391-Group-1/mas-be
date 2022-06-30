0bbe7c4c9ba75042af4db15e22244ffa6d47a014
NguyenTheNhan <nhanntse140758@fpt.edu.vn>
2022-06-30 23:02:04 +0700

    update request user

--- src/MAS.Core/Dtos/Incoming/MasUser/IsActiveChangeRequest.cs
+++ src/MAS.Core/Dtos/Incoming/MasUser/IsActiveChangeRequest.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using MAS.Core.Dtos.Incoming.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace MAS.Core.Dtos.Incoming.MasUser;
 
-public class IsActiveChangeRequest
+public class IsActiveChangeRequest : BaseUpdateRequest
 {
     [Required]
     public bool IsActive { get; set; }
     public string Note { get; set; }
 
-    public DateTime CreatedDate = DateTime.UtcNow.AddHours(7);
+    //public DateTime CreatedDate = DateTime.UtcNow.AddHours(7);
 }
