cf838cd06e978c8b9461e2fca910c199ec7ad0f3
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-07-02 17:36:21 +0700

    Update BaseEntity

--- src/MAS.Core/Entities/Abtraction/BaseEntity.cs
+++ src/MAS.Core/Entities/Abtraction/BaseEntity.cs
@@ -4,7 +4,7 @@ namespace MAS.Core.Entities.Abtraction;
 
 public abstract class BaseEntity
 {
-    public string Id { get; set; } = Guid.NewGuid().ToString();
+    public string Id { get; set; }
     public DateTime CreateDate { get; set; }
     public DateTime? UpdateDate { get; set; }
     public bool IsActive { get; set; }
