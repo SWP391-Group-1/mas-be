a8ddfa5d5afc511e751b38c6db9f533180a5a1d7
Nguyen Nhut Hao <nnhao9a3@gmail.com>
2022-07-14 20:45:33 +0700

    Update Create Slot

--- src/MAS.Infrastructure/Repositories/Slot/SlotRepository.cs
+++ src/MAS.Infrastructure/Repositories/Slot/SlotRepository.cs
@@ -76,21 +76,21 @@ public class SlotRepository : BaseRepository, ISlotRepository
             return result;
         }
 
-        if (request.StartTime < request.CreateDate) {
+        if (request.StartTime < request.CreateDate.AddHours(-7)) {
             result.Error = ErrorHelper.PopulateError((int)ErrorCodes.BadRequest,
                                                      ErrorTypes.BadRequest,
                                                      "Start time must be greater than now!");
             return result;
         }
 
-        if (request.CreateDate > request.StartTime.AddHours(-2)) {
+        if (request.CreateDate.AddHours(-7) > request.StartTime.AddHours(-2)) {
             result.Error = ErrorHelper.PopulateError((int)ErrorCodes.BadRequest,
                                                      ErrorTypes.BadRequest,
                                                      "This slot must be started after now at least 2 hours!");
             return result;
         }
 
-        if (request.CreateDate.AddYears(1) < request.StartTime) {
+        if (request.CreateDate.AddHours(-7).AddYears(1) < request.StartTime) {
             result.Error = ErrorHelper.PopulateError((int)ErrorCodes.BadRequest,
                                                      ErrorTypes.BadRequest,
                                                      "A slot must be created within 1 year!");
